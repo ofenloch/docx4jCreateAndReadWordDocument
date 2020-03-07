1d2065baa4593dfd730fa3962b3ee6aa866ef522
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-03-07 10:27:38 +0100

    use new class createWordDocument to create a sample Word document

--- src/main/java/de/ofenloch/docx4j/examples/App.java
+++ src/main/java/de/ofenloch/docx4j/examples/App.java
@@ -1,13 +1,29 @@
 package de.ofenloch.docx4j.examples;
 
+import java.io.File;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String fileName = "HelloWorld.docx";
+
+        try {
+            WordprocessingMLPackage wmlp = createWordDocument.createSampleWordDocument();
+            wmlp.save(new File(fileName));
+        } catch (InvalidFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Docx4JException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
