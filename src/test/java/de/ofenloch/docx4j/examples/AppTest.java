802df681ae1c4e3eefc9cfbbfa6e1cc59e883c24
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-03-07 16:41:13 +0100

    add basic test

--- src/test/java/de/ofenloch/docx4j/examples/AppTest.java
+++ src/test/java/de/ofenloch/docx4j/examples/AppTest.java
@@ -1,20 +1,33 @@
 package de.ofenloch.docx4j.examples;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test_createDocument() {
+        try {
+            WordprocessingMLPackage wmlp = createWordDocument.createSampleWordDocument();
+            assertNotNull("WordprocessingMLPackage is null", wmlp);
+            File tempFile = File.createTempFile("hello", ".docx");
+            wmlp.save(tempFile);
+            assertTrue("temp file is not readable", tempFile.canRead());
+        } catch (IOException | Docx4JException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            assertTrue(e.getMessage(), false);
+        }
     }
 }
