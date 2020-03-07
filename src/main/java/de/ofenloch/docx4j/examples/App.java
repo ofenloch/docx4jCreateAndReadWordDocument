e981e4d04b969c633b84b2cea9b1000474378094
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-03-07 09:48:34 +0100

    initial commit of the generated maven project

---
+++ src/main/java/de/ofenloch/docx4j/examples/App.java
@@ -0,0 +1,13 @@
+package de.ofenloch.docx4j.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
