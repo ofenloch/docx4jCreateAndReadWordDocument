ea24fa498db042235434c7ef6ea2d2a38d05a559
Oliver Ofenloch <oliver.ofenloch@email.de>
2020-03-10 07:26:13 +0100

    try to extend test

--- src/test/java/de/ofenloch/docx4j/examples/AppTest.java
+++ src/test/java/de/ofenloch/docx4j/examples/AppTest.java
@@ -1,11 +1,13 @@
 package de.ofenloch.docx4j.examples;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
 
+import org.docx4j.Docx4J;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
@@ -24,6 +26,11 @@ public class AppTest {
             File tempFile = File.createTempFile("hello", ".docx");
             wmlp.save(tempFile);
             assertTrue("temp file is not readable", tempFile.canRead());
+            
+            WordprocessingMLPackage wmlpTest = Docx4J.load(tempFile);
+            // This fails! We need to do a differen comparison?
+            // assertEquals(wmlp, wmlpTest);
+
         } catch (IOException | Docx4JException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
